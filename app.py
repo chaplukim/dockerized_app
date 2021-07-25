28777170997303362564a864e023dd5228d2dd6f
Roy Toledano <roy.toled@gmail.com>
2021-07-25 19:51:51 +0300

    hi

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 import pandas as pd
 import numpy as np
@@ -6,6 +7,7 @@ from flask import request
 
 # the name of the app(flask()):
 app = Flask(__name__)
+print(os.path.abspath(os.curdir))
 MODEL = pickle.load(open('churn_model.pkl', 'rb'))
 
 
@@ -16,11 +18,11 @@ def hello_world():
 
 @app.route("/predict_churn", methods=["GET"])
 def get_prediction():
-    is_male = request.args.get("is_male")
-    num_inters = request.args.get("num_inters")
-    late_on_payment = request.args.get("late_on_payment")
-    age = request.args.get("age")
-    years_in_contract = request.args.get("years_in_contract")
+    is_male = request.args.get("is_male", default=1)
+    num_inters = request.args.get("num_inters",default=1)
+    late_on_payment = request.args.get("late_on_payment",default=1)
+    age = request.args.get("age", default=40)
+    years_in_contract = request.args.get("years_in_contract", default=5)
     request_data = [[is_male, num_inters, late_on_payment, age, years_in_contract]]
 
     return str(MODEL.predict(request_data)[0])
@@ -28,5 +30,5 @@ def get_prediction():
 
 # Running from pycharm the code instead of terminal
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
